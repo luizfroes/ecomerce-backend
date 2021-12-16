7201f32a3d26e251a1a1ce5e47c6542f8f5c55e2
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-16 03:51:15 +0000

    connect the models and seeds

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,45 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: {
+    name: "categoryId",
+  },
+});
+//Product.hasOne(Category);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: {
+    name: "categoryId",
+  },
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: {
+    name: "tagId",
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: {
+    name: "productId",
+  },
+});
 
 module.exports = {
   Product,
--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,25 +1,30 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
 
-const sequelize = require('../config/connection');
+const connection = require("../config/connection");
+const { logError } = require("../utils/logger");
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  try {
+    await connection.sync({ force: true });
+    console.log("\n----- DATABASE SYNCED -----\n");
+  } catch (error) {
+    logError("BD connection", error.message);
+  }
+
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log("\n----- CATEGORIES SEEDED -----\n");
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log("\n----- PRODUCTS SEEDED -----\n");
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log("\n----- TAGS SEEDED -----\n");
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
-
+  console.log("\n----- PRODUCT TAGS SEEDED -----\n");
   process.exit(0);
 };
 
