3d761aff16a1eca6a8ff0ba88560bd4f73805b4e
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-16 18:33:31 +0000

    added getAllCategories function

--- src/controllers/api/category.js
+++ src/controllers/api/category.js
@@ -1,27 +1,69 @@
-const getAllCategories = (req, res) => {
-  res.send("getAllCategories");
-  // find all categories
+const { logError } = require("../../utils/logger");
+
+const { Category, Product } = require("../../models");
+
+const getAllCategories = async (req, res) => {
+  try {
+    // find all categories
+    const data = await Category.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    return res.json({ success: true, data });
+  } catch (error) {
+    logError("GET Categories", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
   // be sure to include its associated Products
 };
 
 const getCategoryById = (req, res) => {
-  res.send("getCategoryById");
+  try {
+  } catch (error) {
+    logError("GET Category by ID", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 };
 
 const createNewCategory = (req, res) => {
-  res.send("createNewCategory");
+  try {
+  } catch (error) {
+    logError("POST Category", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
   // create a new category
 };
 
 const updateCategoryById = (req, res) => {
-  res.send("updateCategoryById");
+  try {
+  } catch (error) {
+    logError("PUT Category", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
   // update a category by its `id` value
 };
 
 const deleteCategoryById = (req, res) => {
-  res.send("deleteCategoryById");
+  try {
+  } catch (error) {
+    logError("  DELETE Category", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
   // delete a category by its `id` value
 };
 
